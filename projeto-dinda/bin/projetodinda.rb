ee23f03adc9486886385f667e7d6f8215e56ab8a
Danilo Calderon <33077054+DaniloCalderon@users.noreply.github.com>
2018-08-10 23:14:34 -0300

    Envio dos arquivos da lista

---
+++ projeto-dinda/bin/projetodinda.rb
@@ -0,0 +1,38 @@
+while true do
+
+  puts "-- Output --"
+  puts
+  puts "Nome:"
+  @nome = gets.chomp
+  puts "E-mail:"
+  @email = gets.chomp
+  puts "Login:"
+  @login = gets.chomp
+  puts "Avatar_url:"
+  @avatar_url = gets.chomp
+  puts "Commits_count:"
+  @commits_count = gets.chomp
+  sleep 0.1
+
+  def dados
+  arquivo = []
+  arquivo << puts
+  arquivo << "#{Time.new}"
+  arquivo << "Nome: #{@nome}".upcase
+  arquivo << "E-mail: #{@email}".upcase
+  arquivo << "Login: #{@login}".upcase
+  arquivo << "Avatar_url: #{@avatar_url}".upcase
+  arquivo << "Commits_count: #{@commits_count}".upcase
+  arquivo << "********************"
+  arquivo << puts
+  end
+
+  x = File.open("teste_dinda_10_08_2018_20-25.txt", "a+")
+  x.puts(dados)
+  x.close
+
+  puts "Dados do Rank (Ordem Decrescente):"
+  puts dados
+  sleep 2
+end
+#para encerrar o processo de testes, precione ctrl+d (Linux/Windows) ou command+d (Mac)
---
+++ projeto-dinda/lib/lista.rb
@@ -0,0 +1,82 @@
+class Lista_commits
+
+  puts "----------------------------"
+  puts "-Lista de Maiores Commiters-"
+  puts "----------------------------"
+  def antonio_carlos
+    puts nome = "Antonio Carlos"
+    puts email = "antonio@compilandosolucao.com.br"
+    puts login = "antoniofilho"
+    puts avatar_url = "https://github.com/antoniofilho"
+    puts commits_count = "66 commits"
+  end
+  def pedro_matiello
+    puts nome = "Pedro Matiello"
+    puts email = "pedro@pmatiello.me"
+    puts login = "pmatiello"
+    puts avatar_url = "https://github.com/pmatiello"
+    puts commits_count = "10 commits"
+  end
+  def daniel_coca
+    puts nome = "Daniel Coca"
+    puts email = "Não Disponível"
+    puts login = "danielcoca"
+    puts avatar_url = "https://github.com/danielcoca"
+    puts commits_count = "8 commits"
+  end
+  def marcio_toshio
+    puts nome = "Marcio Toshio"
+    puts email = "contato@marciotoshio.com.br"
+    puts login = "marciotoshio"
+    puts avatar_url = "https://github.com/marciotoshio"
+    puts commits_count = "4 commits"
+  end
+  def kassio_borges
+    puts nome = "kassio Borges"
+    puts email = "kassioborges@gmail.com"
+    puts login = "kassio"
+    puts avatar_url = "https://github.com/kassio"
+    puts commits_count = "3 commits"
+  end
+  def alexandre_santos
+    puts nome = "Alexandre Santos"
+    puts email = "alexandre.sns@gmail.com"
+    puts login = "J133Y"
+    puts avatar_url = "https://github.com/J133Y"
+    puts commits_count = "2 commits"
+  end
+  def bruno_vicenzo
+    puts nome = "Bruno Vicenzo"
+    puts email = "Não Disponível"
+    puts login = "bvicenzo"
+    puts avatar_url = "https://github.com/bvicenzo"
+    puts commits_count = "2 commits"
+  end
+  def rodrigo_brancher
+    puts nome = "Rodrigo Brancher"
+    puts email ="rbrancher@gmail.com"
+    puts login ="rbrancher"
+    puts avatar_url = "https://github.com/rbrancher"
+    puts commits_count = "1 commit"
+  end
+  def lucas_mazza
+    puts nome = "Lucas Mazza"
+    puts email = "Não Disponível"
+    puts login = "lucasmazza"
+    puts avatar_url = "https://github.com/lucasmazza"
+    puts commits_count = "1 commit"
+  end
+end
+
+l1 = Lista_commits.new
+puts l1.antonio_carlos
+puts l1.pedro_matiello
+puts l1.daniel_coca
+puts l1.marcio_toshio
+puts l1.kassio_borges
+puts l1.alexandre_santos
+puts l1.bruno_vicenzo
+puts l1.rodrigo_brancher
+puts l1.lucas_mazza
+
+puts "---------------------------------"
