e24b35f1d410e9b2c1aa4ad75992f8dc9267ecbb
Danilo Calderon <33077054+DaniloCalderon@users.noreply.github.com>
2018-08-10 23:38:00 -0300

    Ajustes de Indentação

--- projeto-dinda/bin/projetodinda.rb
+++ projeto-dinda/bin/projetodinda.rb
@@ -3,28 +3,28 @@ while true do
   puts "-- Output --"
   puts
   puts "Nome:"
-  @nome = gets.chomp
+   @nome = gets.chomp
   puts "E-mail:"
-  @email = gets.chomp
+    @email = gets.chomp
   puts "Login:"
-  @login = gets.chomp
+    @login = gets.chomp
   puts "Avatar_url:"
-  @avatar_url = gets.chomp
+    @avatar_url = gets.chomp
   puts "Commits_count:"
-  @commits_count = gets.chomp
+    @commits_count = gets.chomp
   sleep 0.1
 
   def dados
-  arquivo = []
-  arquivo << puts
-  arquivo << "#{Time.new}"
-  arquivo << "Nome: #{@nome}".upcase
-  arquivo << "E-mail: #{@email}".upcase
-  arquivo << "Login: #{@login}".upcase
-  arquivo << "Avatar_url: #{@avatar_url}".upcase
-  arquivo << "Commits_count: #{@commits_count}".upcase
-  arquivo << "********************"
-  arquivo << puts
+    arquivo = []
+    arquivo << puts
+    arquivo << "#{Time.new}"
+    arquivo << "Nome: #{@nome}".upcase
+    arquivo << "E-mail: #{@email}".upcase
+    arquivo << "Login: #{@login}".upcase
+    arquivo << "Avatar_url: #{@avatar_url}".upcase
+    arquivo << "Commits_count: #{@commits_count}".upcase
+    arquivo << "********************"
+    arquivo << puts
   end
 
   x = File.open("teste_dinda_10_08_2018_20-25.txt", "a+")
